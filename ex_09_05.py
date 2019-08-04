41b43ac4e7427171617436e2b0a68e413a834887
Kaouther GSC <45854599+kaoutherstclair@users.noreply.github.com>
2019-08-04 09:35:30 +0100

    Add files via upload

--- ex_09_05.py
+++ ex_09_05.py
@@ -1,3 +1,4 @@
+#This code reads in a file, finds a line starting with specific criteria,splits it to return the right end of the line(string):
 inp=input ('Enter file name: ')
 try:
     h=open (inp)
@@ -10,12 +11,30 @@ for line in h:
     #words=line.split()
     if line.startswith ('From:'):
         #print (line)
-        delimiter='@'
-        t=line.split(delimiter)
+        #delimiter='@'
+        t=line.split('@')## uname,domain = line.split('@')
         #print (t[1])
         domains_list.append (t[1])
 #print (domains_list)
 d={}
 for domain in domains_list:
     d [domain]=d.get (domain,0)+1
+print (d)
+#Another option would be:
+inp=input ('Enter file name: ')
+try:
+    h=open (inp)
+except:
+    print('Invalid input !')
+    exit()
+d={}
+for line in h:
+    line=line.rstrip()
+    #words=line.split()
+    if line.startswith ('From:'):
+        #print (line)
+        #delimiter='@'
+        uname,domain = line.split('@')##creates a tuple (uname,domain)
+        #print ((uname,'***',domain))
+        d [domain]=d.get (domain,0)+1
 print (d)
